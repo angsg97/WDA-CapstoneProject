84437125a234e017de83271b65514a5d99807a47
Ang Song Gee <ancientspeargun69@gmail.com>
2020-05-28 11:05:10 +0000

    Added service worker js files to cache files to the browser storage. Also allows program to fetch those files from the cache when offline and when available. Also added notifications bar to allow user to accept the update from the service worker

--- js/script.js
+++ js/script.js
@@ -1,23 +1,29 @@
-let navbarBurger = document.getElementById('navbar-burger');
-// navbarBurger.addEventListener('click', closeNavbar);
 
-document.addEventListener('click', (event) => {
-  console.log(event.target.classList);
-  if (navbarBurger.classList.contains('is-active')) {
-    console.log("active");
-    //check whether you are still clicking in one of the navbar components
-    if (!event.target.classList.contains('navbar-menu') && !event.target.classList.contains('navbar-item')) {
-      toggleNavbar();
+let navbarBurger = document.getElementById('navbar-burger');
+
+document.addEventListener('DOMContentLoaded', () => {
+  // navbarBurger.addEventListener('click', closeNavbar);
+
+  document.addEventListener('click', (event) => {
+    console.log(event.target.classList);
+    if (navbarBurger.classList.contains('is-active')) {
+      console.log("active");
+      //check whether you are still clicking in one of the navbar components
+      if (!event.target.classList.contains('navbar-menu') && !event.target.classList.contains('navbar-item')) {
+        toggleNavbar();
+      }
     }
-  }
-  // Navbar is not already active so check if user is clicking on the navbar
-  else {
-    if (event.target.classList.contains('navbar-burger')){
-      toggleNavbar();
+    // Navbar is not already active so check if user is clicking on the navbar
+    else {
+      if (event.target.classList.contains('navbar-burger')){
+        toggleNavbar();
+      }
     }
-  }
-  
+    
+  })
 })
+
+
 
 function toggleNavbar () {
   const target = navbarBurger.dataset.target;
---
+++ sw.js
@@ -0,0 +1,91 @@
+let cacheName = '28/5/20_Cache8';
+// let caches;
+let filesToCache = [
+  '/',
+  'index.html',
+  'about-me.html',
+  'contact-me.html',
+  'projects.html',
+  'css/arrow.css',
+  'css/debug.css',
+  'css/style.css',
+  'images/icons/icon-128px.png',
+  'images/icons/icon-144px.png',
+  'images/icons/icon-152px.png',
+  'images/icons/icon-192px.png',
+  'images/icons/icon-256px.png',
+  'images/icons/icon-512px.png',
+  'images/avatar-256by256.png',
+  'images/avatar-512by512.svg',
+  'images/Blackjack_640by320.png',
+  'images/Calculator_640by320.jpg',
+  'images/Portfolio_640by320.png',
+  'js/script.js',
+  'public/manifest.json'
+]
+
+/* 
+start the service worker, when the user access
+the website online. This will add the all the files 
+listed in filesToCache to the browser cache.
+*/
+self.addEventListener('install', function(e){
+  console.log("on install");
+  console.log(cacheName);
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache){
+      console.log("Adding files to cache");
+      return cache.addAll(filesToCache);
+    })
+  )
+
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  // self.skipWaiting();
+})
+
+// self.addEventListener('activate', (evt) => {
+//   console.log('[ServiceWorker] Activate');
+//   // CODELAB: Remove previous cached data from disk.
+//   evt.waitUntil(
+//     caches.keys().then((keyList) => {
+//       return Promise.all(keyList.map((key) => {
+//         if (key !== cacheName) {
+//           console.log('[ServiceWorker] Removing old cache', key);
+//           return caches.delete(key);
+//         }
+//       }));
+//     })
+//   );
+
+//   self.clients.claim();
+// });
+
+/*
+If offline or if the file exists in the cache, then it will fetch the files from cache
+*/
+self.addEventListener('fetch', function(e){
+  e.respondWith(
+    caches.match(e.request,{
+        cacheName: cacheName
+    }).then(function(response){
+        console.log(response);
+        console.log("Fetching "+e.request.url);
+      return response || fetch (e.request)
+    })
+  )
+})
+
+self.addEventListener('message', function (event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
